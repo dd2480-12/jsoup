3d3583222333a5c0878c66cc73c892cbc5c1262d
jlundhol <jlundhol@kth.se>
2020-02-17 12:37:36 +0100

    Add test for unhandled token

--- src/test/java/org/jsoup/select/QueryParserTest.java
+++ src/test/java/org/jsoup/select/QueryParserTest.java
@@ -63,4 +63,8 @@ public class QueryParserTest {
         Evaluator parse = QueryParser.parse(" span div  ");
         assertEquals("div :parentspan", parse.toString()); // TODO - don't really love that toString() result...
     }
+
+    @Test(expected = Selector.SelectorParseException.class) public void invalidToken() {
+        QueryParser.parse(":qwe");
+    }
 }
