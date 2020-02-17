ca8f6b325c58e2bb9be6641896a6af48c3a9ec72
Alex <alexdiaz.jdz@gmail.com>
2020-02-17 22:26:04 +0100

    add tests for setting correct mode in HtmlTreeBuilder, #9

--- src/test/java/org/jsoup/parser/HtmlTreeBuilderTest.java
+++ src/test/java/org/jsoup/parser/HtmlTreeBuilderTest.java
@@ -1,10 +1,13 @@
 package org.jsoup.parser;
 
+import org.jsoup.nodes.Element;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class HtmlTreeBuilderTest {
     @Test
@@ -24,5 +27,37 @@ public class HtmlTreeBuilderTest {
             Arrays.sort(array);
             assertArrayEquals(array, copy);
         }
+    }
+
+    @Test
+    public void treeCorrectlyTransitionsToLastElement() {
+        HtmlTreeBuilder tree = new HtmlTreeBuilder();
+        tree.setContextElement(new Element("select"));
+
+        ArrayList<Element> stack = new ArrayList<>();
+        stack.add(new Element("dummy"));
+        tree.stack = stack;
+
+        tree.resetInsertionMode();
+        assertEquals(tree.state(), HtmlTreeBuilderState.InSelect);
+    }
+
+    @Test
+    public void treeCorrectlyTransitionsToElements() {
+        HtmlTreeBuilder tree = new HtmlTreeBuilder();
+        ArrayList<Element> stack = new ArrayList<>();
+        stack.add(new Element("dummy"));
+        tree.stack = stack;
+
+        Element[] elements = { new Element("select"), new Element("caption"), new Element("tr") };
+        HtmlTreeBuilderState[] states = { HtmlTreeBuilderState.InSelect, HtmlTreeBuilderState.InCaption,
+                HtmlTreeBuilderState.InRow };
+
+        for (int i = 0; i < elements.length; i++) {
+            tree.setContextElement(elements[i]);
+            tree.resetInsertionMode();
+            assertEquals(tree.state(), states[i]);
+        }
+
     }
 }
